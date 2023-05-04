ee00a0dd4e2c53089dff6739ef3a47242c31c2bc
Joy Wanjiru <70502261+Joy879@users.noreply.github.com>
2023-05-04 22:14:27 +0300

    Add fastapi code

--- FASTAPI/app.py
+++ FASTAPI/app.py
@@ -1,1 +1,25 @@
+# 1. Library imports
+import pandas as pd
+from pycaret.regression import load_model, predict_model
+from fastapi import FastAPI
+import uvicorn
 
+# 2. Create the app object
+app = FastAPI()
+
+#. Load trained Pipeline
+# loading the trained model diamond-pipeline from your disk (Your script must be in the same folder as the file).
+model = load_model('diamond-pipeline')
+
+# Define predict function
+#  defining a function called predict which will take the input and internally uses PyCaret’s predict_model function to generate predictions and return the value as a dictionary
+@app.post('/predict')
+def predict(carat_weight, cut, color, clarity, polish, symmetry, report):
+    data = pd.DataFrame([[carat_weight, cut, color, clarity, polish, symmetry, report]])
+    data.columns = ['Carat Weight', 'Cut', 'Color', 'Clarity', 'Polish', 'Symmetry', 'Report']
+
+    predictions = predict_model(model, data=data) 
+    return {'prediction': int(predictions['Label'][0])}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
