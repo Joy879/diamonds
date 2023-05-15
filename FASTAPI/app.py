00978bb28c57ad56998e175443d8c48c1b35c260
Joy Wanjiru <70502261+Joy879@users.noreply.github.com>
2023-05-15 11:38:50 +0300

    Update app.py

--- FASTAPI/app.py
+++ FASTAPI/app.py
@@ -1,6 +1,6 @@
 # 1. Library imports
 import pandas as pd
-from pycaret.regression import load_model, predict_model
+import pickle
 from fastapi import FastAPI
 import uvicorn
 
@@ -9,7 +9,7 @@ app = FastAPI()
 
 #. Load trained Pipeline
 # loading the trained model diamond-pipeline from your disk (Your script must be in the same folder as the file).
-model = load_model('diamond-pipeline')
+model = pickle.load(open("diamond_prediction.pkl", "rb"))
 
 # Define predict function
 #  defining a function called predict which will take the input and internally uses PyCaret’s predict_model function to generate predictions and return the value as a dictionary
@@ -18,8 +18,8 @@ def predict(carat_weight, cut, color, clarity, polish, symmetry, report):
     data = pd.DataFrame([[carat_weight, cut, color, clarity, polish, symmetry, report]])
     data.columns = ['Carat Weight', 'Cut', 'Color', 'Clarity', 'Polish', 'Symmetry', 'Report']
 
-    predictions = predict_model(model, data=data) 
-    return {'prediction': int(predictions['Label'][0])}
+    predictions = model.predict(data) 
+    return {'prediction': predictions[0]}
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
